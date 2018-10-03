2c96b65b06f7ef3646fe2325013c45f932b650df
AlanJIE <934797007@qq.com>
2018-10-03 14:44:40 +0800

    I can't stand!

---
+++ script.js
@@ -0,0 +1,42 @@
+var qrqq=document.getElementById('qrqq');
+var qrWechat=document.getElementById('qrWechat');
+var qrGithub=document.getElementById('qrGithub');
+
+var iconqq=document.getElementById('iconqq');
+var iconWechat=document.getElementById('iconWechat');
+var iconGithub=document.getElementById('iconGithub');
+
+function qqIn(){
+	qrqq.style.display="block";
+}
+
+function qqOut(){
+	qrqq.style.display="none";
+}
+
+function wechatIn(){
+	qrWechat.style.display="block";
+}
+
+function wechatOut(){
+	qrWechat.style.display="none";
+}
+
+function githubIn(){
+	qrGithub.style.display="block";
+}
+
+function githubOut(){
+	qrGithub.style.display="none";
+}
+
+// 内容切换
+	var index=document.getElementById("index");
+
+	function firstDay(){
+		index.src="./content/firstDay.html"
+	}
+
+	function secondDay(){
+		index.src="./content/secondDay.html"
+	}
